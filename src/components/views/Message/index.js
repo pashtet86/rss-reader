afeb474b87d92b6b98b895a5452bdf2b78277920
Pavel Bazhenov <bazhenovpavel@gmail.com>
2020-06-01 22:13:54 +0300

    ui improvements

--- src/components/views/Message/index.js
+++ src/components/views/Message/index.js
@@ -4,12 +4,6 @@ import Button from '@material-ui/core/Button';
 
 const Message = ({ feedItem }) => {
 
-  // if (feedItem.content) {
-  //   return <div dangerouslySetInnerHTML={{ __html: feedItem.content }} />
-  // } else {
-  //   return <Button variant="contained" color="primary" href={feedItem.link} rel="noopener noreferrer">Open in new tab</Button>
-  // }
-
   return (
     <div className={`message-details ${feedItem.title ? '' : 'no-data'}`}>
       {feedItem.content &&
