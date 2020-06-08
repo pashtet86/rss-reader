48e1b0e473c0db0f46c6d8464afc754070de2739
Pavel Bazhenov <bazhenovpavel@gmail.com>
2020-06-08 22:26:53 +0300

    position fixes for diagramm

--- src/components/TheLettersCounter.js
+++ src/components/TheLettersCounter.js
@@ -9,7 +9,7 @@ class TheLettersCounter extends React.Component  {
     super();
     this.state = {
       lowerCaseAlphabet: 'abcdefghijklmnopqrstuvwxyz',
-      letterCounts: [{ title: 'test', value: 11, color: '#000' }],
+      letterCounts: [],
     };
   }
 
@@ -18,7 +18,6 @@ class TheLettersCounter extends React.Component  {
     const { text } = this.props;
     const letters = lowerCaseAlphabet;
     const letterCounts = [];
-    // const text = 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?';
     const textLength = text.length;
 
     const t1 = performance.now()
@@ -65,7 +64,7 @@ class TheLettersCounter extends React.Component  {
     };
 
     return (
-      <div>
+      <div className="letters-statistic">
         <PieChart
           viewBoxSize={[120, 120]}
           center={[60, 60]}
